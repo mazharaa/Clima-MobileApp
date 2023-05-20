6c802c6483362084835f3734b31703c503c1ba08
mazharaa <mazharaa32@gmail.com>
2023-05-20 09:25:11 +0700

    Geolocator Error #1 solved

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -9,6 +9,32 @@ class LoadingScreen extends StatefulWidget {
 }
 
 void getLocation() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+  if(!serviceEnabled) {
+    return Future.error('Location service are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+
+  if(permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+
+    if(permission == LocationPermission.denied) {
+      return Future.error('Location permission are denied.');
+    }
+  }
+
+  if(permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permission are permanently denied, '
+            'we cannot request permission.'
+    );
+  }
+
   Position position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.low
   );
