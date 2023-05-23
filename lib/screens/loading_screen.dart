3fee44240a5aed7b48571b8dcd2b513198326d24
mazharaa <mazharaa32@gmail.com>
2023-05-23 20:09:37 +0700

    getLocation method called in initState method

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -8,40 +8,46 @@ class LoadingScreen extends StatefulWidget {
   State<LoadingScreen> createState() => _LoadingScreenState();
 }
 
-void getLocation() async {
-  bool serviceEnabled;
-  LocationPermission permission;
-
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-
-  if(!serviceEnabled) {
-    return Future.error('Location service are disabled.');
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
   }
 
-  permission = await Geolocator.checkPermission();
+  void getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
-  if(permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if(!serviceEnabled) {
+      return Future.error('Location service are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
 
     if(permission == LocationPermission.denied) {
-      return Future.error('Location permission are denied.');
+      permission = await Geolocator.requestPermission();
+
+      if(permission == LocationPermission.denied) {
+        return Future.error('Location permission are denied.');
+      }
     }
-  }
 
-  if(permission == LocationPermission.deniedForever) {
-    return Future.error(
-        'Location permission are permanently denied, '
-            'we cannot request permission.'
+    if(permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permission are permanently denied, '
+              'we cannot request permission.'
+      );
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low
     );
+    print(position);
   }
 
-  Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.low
-  );
-  print(position);
-}
-
-class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
