cafeca1b7130b26863987a69db38adb047dd53d8
mazharaa <mazharaa32@gmail.com>
2023-05-24 22:26:12 +0700

    Create methode getData() from openweather.com API and get error on the response object.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -13,6 +14,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -20,6 +22,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     print(location.latitude);
     print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+      'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon='
+      '139&appid=b6907d289e10d714a6e88b30761fae22');
+    print(response.statusCode);
   }
 
   @override
