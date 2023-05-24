67e22dbc4973546d34d593cdbc2c2c18d790b960
mazharaa <mazharaa32@gmail.com>
2023-05-24 22:47:35 +0700

    Fix error http response get method

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -26,8 +26,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getData() async {
     http.Response response = await http.get(
-      'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon='
-      '139&appid=b6907d289e10d714a6e88b30761fae22');
+      Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon='
+        '139&appid=b6907d289e10d714a6e88b30761fae22')
+    );
     print(response.statusCode);
   }
 
